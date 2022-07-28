fb0a7efb7e8ff2006675f49210e2cb5426d5bf8d
Ramco <ramazane791@gmail.com>
2022-07-28 22:21:56 +0300

    fix fetch uri

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -13,7 +13,7 @@ export default function Preview() {
     }
 
     const getImage = async () => {
-        const image = await axios.get("https://ramcho.xyz/i/" + id, { withCredentials: true })
+        const image = await axios.get("https://api.ramcho.xyz/i/" + id, { withCredentials: true })
         if (!image.data.error) {
             setImgData(image.data)
         }
