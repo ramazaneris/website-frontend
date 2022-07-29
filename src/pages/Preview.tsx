1538a788546246d019d4c976e98781e591b97d24
Ramco <ramazane791@gmail.com>
2022-07-29 23:25:55 +0300

    Changed the fetch data

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -2,84 +2,61 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { useParams } from 'react-router-dom'
-export default function Preview() {
+export default function Preview(props: { getInfo: any }) {
     const { id } = useParams()
     const [imgData, setImgData] = useState<any>(null)
-    const [loaded, setLoaded] = useState(false)
 
-
-    function timeout(ms: number) {
-        return new Promise((resolve: any) => setTimeout(resolve, ms));
+    const setImageData = async () => {
+        setImgData(await props.getInfo(id))
     }
-
-    const getImage = async () => {
-        const image = await axios.get("https://api.ramcho.xyz/i/" + id, { withCredentials: true })
-        if (!image.data.error) {
-            setImgData(image.data)
-        }
-    }
-
     useEffect(() => {
-        getImage()
-    }, [])
-
-    useEffect(() => {
-        setLoaded(true)
-    }, [imgData])
-
+        setImageData()
+    })
     return (
         <div>
-            {loaded ? (
-                <>
-                    {imgData && (<>
-                        <Helmet>
-                            <meta property="og:title" content={`${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
-                            <meta property="og:url" content={`https://ramcho.xyz/u/${imgData?.filename}`} />
-                        </Helmet>
-                        {imgData?.type.split("/")[0] === "video" ? (
-                            <Helmet>
-                                <meta property="og:video" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
-                                <meta property="og:type" content="video" />
-                            </Helmet>
-                        ) : (<Helmet>
-                            <meta name="twitter:image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
-                            <meta itemProp="image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
-                            <meta property="og:image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
-                            <meta property="og:image:width" content="400" />
-                            <meta name="twitter:image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
-                            <meta content="summary_large_image" name="twitter:card" />
-                        </Helmet>)}
-
-                    </>)}
-                    <div className="h-screen w-screen">
-                        <div className="flex relative justify-center items-center h-full w-full">
-                            <div className="absolute">
-                                <div className="md:w-[500px] md:h-[500px] bg-[#00000050] rounded-lg">
-                                    <div className="flex justify-center items-center h-full w-full">
-                                        <div className="absolute">
-                                            {imgData ? (
-                                                <>
-                                                    <div className="bg-black py-2 rounded-xl px-2">
-                                                        <div className="p-2 text-white">
-                                                            {imgData?.filename + '.' + imgData?.type.split("/")[1]}
-                                                        </div>
-                                                        <div className="items-center mb-8">
-                                                            <img src={imgData?.url} className="max-w-[400px] w-[400px] max-h-[400px]" alt="" />
-                                                        </div>
-                                                    </div>
-                                                </>
-
-                                            ) : (<>Image not found</>)}
-                                        </div>
-                                    </div>
+            {imgData && (<>
+                <Helmet>
+                    <meta property="og:title" content={`${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
+                    <meta property="og:url" content={`https://ramcho.xyz/u/${imgData?.filename}`} />
+                </Helmet>
+                {imgData?.type.split("/")[0] === "video" ? (
+                    <Helmet>
+                        <meta property="og:video" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
+                        <meta property="og:type" content="video" />
+                    </Helmet>
+                ) : (<Helmet>
+                    <meta name="twitter:image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
+                    <meta itemProp="image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
+                    <meta property="og:image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
+                    <meta property="og:image:width" content="400" />
+                    <meta name="twitter:image" content={`https://api.ramcho.xyz/u/${imgData?.filename}.${imgData?.type.split("/")[1]}`} />
+                    <meta content="summary_large_image" name="twitter:card" />
+                </Helmet>)}
+            </>)}
+            <div className="h-screen w-screen">
+                <div className="flex relative justify-center items-center h-full w-full">
+                    <div className="absolute">
+                        <div className="md:w-[500px] md:h-[500px] bg-[#00000050] rounded-lg">
+                            <div className="flex justify-center items-center h-full w-full">
+                                <div className="absolute">
+                                    {imgData ? (
+                                        <>
+                                            <div className="bg-black py-2 rounded-xl px-2">
+                                                <div className="p-2 text-white">
+                                                    {imgData?.filename + '.' + imgData?.type.split("/")[1]}
+                                                </div>
+                                                <div className="items-center mb-8">
+                                                    <img src={imgData?.url} className="max-w-[400px] w-[400px] max-h-[400px]" alt="" />
+                                                </div>
+                                            </div>
+                                        </>
+                                    ) : (<>Image not found</>)}
                                 </div>
                             </div>
-
                         </div>
                     </div>
-                </>
-            ) : (<>wait</>)
-            }
+                </div>
+            </div>
         </div >
     )
 }
