b03f58b09b7029205716d28e27cf19ad413a38d9
Ramco <ramazane791@gmail.com>
2022-08-09 21:00:52 +0300

    fix: profile photo url

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -6,7 +6,7 @@ export default function ProfileCard() {
     <div className="md:w-[500px] md:h-[500px] bg-[#00000050] shadow-2xl rounded-2xl">
       <div className="flex justify-center items-center h-full w-full">
         <div className="absolute">
-          <div className="items-center mb-8"><img src="https://api.ramcho.xyz/u/r9m9x0ob8n.png" className="rounded-full w-60" alt="" /></div>
+          <div className="items-center mb-8"><img src="https://api.ramcho.xyz/cdn/r9m9x0ob8n.png" className="rounded-full w-60" alt="" /></div>
           <div className="text-center">
             <div>
               <h1 className="text-[32px]">Ramazan Eriş</h1>
